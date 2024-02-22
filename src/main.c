1d3e820a5f2e1a9f4b8cfa3fc561b0c2c78c55aa
Ethan Diosana <ethandiosana1@gmail.com>
2024-02-22 10:24:35 -0800

    First commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stringTools.h>
+
+int main(int argc, const char *argv[])
+{
+    // Goal: Create a remote shell app.
+    printf("argc: %d argv: %s", argc, argv[0]);
+
+    // Create a process manager.
+
+    printf("Hello world!\n");
+    return EXIT_SUCCESS;
+}
